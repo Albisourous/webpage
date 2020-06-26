5f73fddadaf3146f89162c0f691dd6bd278479de
Albin Shrestha <shrestha_albin@yahoo.com>
2020-06-25 22:31:06 -0500

    iss1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Router basename={process.env.PUBLIC_URL}>< App /></Router>,
   document.getElementById('root')
 );
 
