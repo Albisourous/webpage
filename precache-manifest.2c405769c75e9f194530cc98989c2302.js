cf146db9360c91db48be694b6b567afdef990d8e
Albin Shrestha <shrestha_albin@yahoo.com>
2020-06-25 21:51:07 -0500

    Updates

---
+++ precache-manifest.2c405769c75e9f194530cc98989c2302.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deae1ef74958251cdeb7d583bdc32596",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd294d4c1ef2f73f54b",
+    "url": "/static/css/main.b9a4367a.chunk.css"
+  },
+  {
+    "revision": "1fa1fccb3b4a36c3fe92",
+    "url": "/static/js/2.fe9e347d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.fe9e347d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd294d4c1ef2f73f54b",
+    "url": "/static/js/main.8a8a3b0b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "6409a7b5f44899b0aff903c1c148533e",
+    "url": "/static/media/albin.6409a7b5.jpg"
+  },
+  {
+    "revision": "9820e1c7a3b41f6a2ee8bf5cade436fc",
+    "url": "/static/media/img1.9820e1c7.jpeg"
+  },
+  {
+    "revision": "96a5638cf8b27a5890a95ed38ea37375",
+    "url": "/static/media/kevin.96a5638c.jpg"
+  },
+  {
+    "revision": "b9b63fdc70b0febb130a67d72a07a434",
+    "url": "/static/media/li.b9b63fdc.jpg"
+  },
+  {
+    "revision": "505002a932f643e9f08be27175648bc5",
+    "url": "/static/media/noah.505002a9.jpg"
+  },
+  {
+    "revision": "1cb3cd0e9fba8406576aec3f79cd5309",
+    "url": "/static/media/yijing.1cb3cd0e.jpeg"
+  }
+]);
